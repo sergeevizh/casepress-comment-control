8cc604a327c041263fc56e81b66e5f8113fb0d59
Anton Tsarenko <tsarenkoanton90@gmail.com>
2015-01-25 21:39:47 +0400

    rename files and add dropdown menu

--- cp-todo-comments.php
+++ cp-todo-comments.php
@@ -1,9 +1,18 @@
 <?php
 /*
-Plugin Name: CasePress. ToDo Comments
-
+Plugin Name:           CasePress. ToDo Comments
+Plugin URI:            http://casepress.org
+Description:           Плагин позволяет при добавлении коментария отметить его галочкой "На контроль". Отмеченные коментарии выводятся в виде списка шорткодом cp_todo_comments, порядок коментариев можно менять перетаскиванием элементов списка (порядок сохраняется). У каждого коментария есть кнопки "Редактировать", "Удалить".
+Author:                CasePress
+Author URI:            http://casepress.org
+Author email:          s@casepress.org
+GitHub Plugin URI:     https://github.com/systemo-biz/todo-comments-cp
+GitHub Branch:         master
+Version:               0.1
 */
-function add_todo_test_cp(){
+
+add_shortcode('cp_todo_comments', 'add_todo_cp');
+function add_todo_cp(){
 global $post;
     ob_start();
     $args = array(
@@ -39,6 +48,25 @@ global $post;
                             </div>
                             <div class="col-md-2">
                                 <div class="hide_hover">
+                                    <div class="dropdown">
+                                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+                                            Действие
+                                            <span class="caret"></span>
+                                        </button>
+                                        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                                            <li role="presentation">
+                                                <a role="menuitem" tabindex="-1" href="#" data-comment_id="<?php echo $com_ID?>" class="delete_li_item">Удалить
+                                                  <!--  <button type="button" data-comment_id="<?php echo $com_ID?>" class="delete_li_item btn btn-default btn-xs">
+                                                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                                    </button> -->
+                                                </a>
+                                            </li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo get_comment_link( $comment->comment_ID );?>">Перейти к комментарию </a></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo get_edit_comment_link( $comment->comment_ID );?>">Редактировать</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                               <!-- <div class="hide_hover">
                                     <button type="button" data-comment_id="<?php echo $com_ID?>" class="delete_li_item btn btn-default btn-xs">
                                         <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
                                     </button>
@@ -47,7 +75,7 @@ global $post;
                                             <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                                         </button>
                                     </a>
-                                </div>
+                                </div>-->
                             </div><!--end col-md-2-->
                         </div><!--end row-->
                     </div><!--end panel-body-->
@@ -59,14 +87,13 @@ global $post;
     </ul>
     <script>
         var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
-        //отправка ajax`ом порядка вывода коментов
-        jQuery(document).ready(function(){
+        jQuery(document).ready(function($){
+            //отправка ajax`ом порядка вывода коментов
             var group = jQuery("ul#todo-comments").sortable({
                 placeholder: '<li class="placeholder" />',
-                handle: 'div.icon-move',
+                handle: 'div.icon-move',                                //дергалка за которую перетаскивать
                 onDrop: function (item, container, _super) {
                     var serialize_data = group.sortable("serialize").get();
-                    console.log(serialize_data);
                     var data = {
                         serialize_data: serialize_data,
                         action: 'cp_control_order_change'
@@ -80,29 +107,10 @@ global $post;
                     });
                     _super(item, container)
                 }
-            })
-        })
-        //изменение меты cp_control_done, отправка данных ajax
-        jQuery(document).ready(function () {
-            jQuery(".lock_comment").change(function () {
-                comment_id = jQuery(this) . attr("data-comment_id")
-                var data = {
-                    comment_id: comment_id,
-                    action: 'cp_change'
-                };
-                jQuery.post(ajaxurl, data, function(response) {
-                    if (response.type == "success") {
-                        // console.log("все прошло хоррошо")
-                    } else {
-                        console.log("Ошибка сохранения результата")
-                    }
-                });
             });
-        })
-        //удаление меты cp_control, запрос ajax
-        jQuery(document).ready( function() {
+            //удаление меты cp_control, запрос ajax
             jQuery(".delete_li_item").click( function() {
-                comment_id = jQuery(this).attr("data-comment_id")
+                comment_id = jQuery(this).attr("data-comment_id");
                 var data = {
                     comment_id: comment_id,
                     action: 'cp_delete'
@@ -115,41 +123,61 @@ global $post;
                     }
                 });
             });
-        })
+            //изменение меты cp_control_done, отправка данных ajax
+            jQuery(".lock_comment").change(function () {
+                comment_id = jQuery(this) . attr("data-comment_id");
+                var data = {
+                    comment_id: comment_id,
+                    action: 'cp_change'
+                };
+                jQuery.post(ajaxurl, data, function(response) {
+                    if (response.type == "success") {
+                        // console.log("все прошло хоррошо")
+                    } else {
+                        console.log("Ошибка сохранения результата")
+                    }
+                });
+            });
+        });
     </script>
     <?php
     return ob_get_clean();
 }
-//добавляем шорткод
-add_shortcode('todo_comments', 'add_todo_test_cp');
-//подключения сортировки
+
+//подключения js-плагина сортировки и css
 add_action('wp_enqueue_scripts', 'load_jquery_sortable');
 function load_jquery_sortable()
 {
     global $post;
     if (!isset ($post)) return;
-    if (has_shortcode($post->post_content, 'todo_comments') or is_single()) {
+    if (has_shortcode($post->post_content, 'cp_todo_comments') or is_single()) {
         wp_enqueue_style('todo', plugin_dir_url(__FILE__) . '/todo.css');
         wp_enqueue_script('jquery-sortable-johny', plugin_dir_url(__FILE__) . '/jquery-sortable-min.js', array('jquery'));
-      // wp_enqueue_script('jquery-sortable', plugin_dir_url(__FILE__) . '/jquery-ui-1.11.2/jquery-ui.js', array('jquery'));
     }
 }
 
+//отключает jquery ui sortable, у плагина jquery-sortable by johny конфликт с ним
 add_action( 'wp_print_scripts', 'de_script', 100 );
-
 function de_script() {
-    wp_dequeue_script( 'jquery-ui-sortable' );
-    wp_deregister_script( 'jquery-ui-sortable' );
+    global $post;
+    if (!isset ($post)) return;
+    if (has_shortcode($post->post_content, 'cp_todo_comments')){
+        wp_dequeue_script( 'jquery-ui-sortable' );
+        wp_deregister_script( 'jquery-ui-sortable' );
+    }
 }
+
 //добавляет чекбокс к форме комментария
 add_action('comment_form', 'cp_control_checkbox');
 function cp_control_checkbox() {
     ?>
-    <p> <input type="hidden" name="check" value="no">
+    <p>
+        <input type="hidden" name="check" value="no">
         <label for="check_for"><input type="checkbox" name="check" value="yes" id="check_for"> На контроль</label>
     </p>
     <?php
 }
+
 //добавляет мета поля к форме комментирования
 add_action('comment_post','cp_control_check');
 function cp_control_check($comment_id){
@@ -158,13 +186,14 @@ function cp_control_check($comment_id){
         add_comment_meta ($comment_id, 'cp_control' ,'yes' ); // если стоит checkbox "На контроль" - мета поле, используемое для вывода коментов в шорткоде
     }
 }
+
 //обработчик ajax изменения порядка
 add_action("wp_ajax_cp_control_order_change", "cp_control_order_change");
 add_action("wp_ajax_nopriv_cp_control_order_change", "cp_control_order_change");
 function cp_control_order_change(){
     $order = $_POST['serialize_data'];
     $i = 0;
-    foreach ($order[0] as $val){ //изза входящего массива данных
+    foreach ($order[0] as $val){ //такая струтура из-за входящего массива данных
         foreach ($val as $value){
             update_comment_meta($value, "cp_control_order", $i);
             $i++;
@@ -173,41 +202,47 @@ function cp_control_order_change(){
     $result['type'] = "success";
     wp_send_json($result);
 }
+
 //обработчик ajax запроса
 add_action("wp_ajax_cp_change", "cp_control_change");
 add_action("wp_ajax_nopriv_cp_change", "cp_control_change");
-function cp_control_change()
-{
+function cp_control_change(){
     /*
     // проверка nonce
     * if ( !wp_verify_nonce( $_REQUEST['nonce'], "my_user_vote_nonce")) {
     exit("No naughty business please");
     }*/
+
     $cp_control_done = get_comment_meta($_REQUEST["comment_id"], "cp_control_done", true);
-//если мета cp_control_done не объявлена создать не заблокированную
-    if($cp_control_done === 'unlock') {
+    //если мета cp_control_done не объявлена создать не заблокированную
+
+    if ($cp_control_done === 'unlock') {
         $cp_control_done = 'lock'; // смена типа
     }
-    elseif($cp_control_done === 'lock'){
+    elseif ($cp_control_done === 'lock'){
         $cp_control_done = 'unlock'; // смена типа
     }
     else{
         $cp_control_done = 'lock'; // в первый раз
     }
+
     $res = update_comment_meta($_REQUEST["comment_id"], "cp_control_done", $cp_control_done);
+
     if ($res === false) {
         $result['type'] = "error";
     } else {
         $result['type'] = "success";
     }
+
     wp_send_json($result);
 }
+
 //обработчик для неавторизированных юзеров
-function my_must_login()
-{
+function my_must_login(){
     echo "You must log in to vote";
     die();
 }
+
 //обработчик удаления cp_control
 add_action("wp_ajax_cp_delete", "my_cp_delete");
 add_action("wp_ajax_nopriv_cp_delete", "my_cp_delete");
